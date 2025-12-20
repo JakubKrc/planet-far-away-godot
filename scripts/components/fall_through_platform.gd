1afcdd6d358b05863c85c1ec2420b87ed7bc1195
JakubKrc <96569372+JakubKrc@users.noreply.github.com>
2025-12-20 17:16:17 +0100

    upgraded falling through platforms
    checks now if colision was in group fall_through_platform

--- scripts/components/fall_through_platform.gd
+++ scripts/components/fall_through_platform.gd
@@ -3,5 +3,21 @@ extends Node
 @onready var parent = get_parent()
 
 func fall():
-	if(parent.is_on_floor()):
-		parent.position.y += 1
+		
+	if not parent.is_on_floor():
+		return
+
+	var collision = parent.get_last_slide_collision()
+	if collision == null:
+		return
+
+	var collider = collision.get_collider()
+	print(collider)
+	if not collider.is_in_group("fall_through_platform"):
+		return
+
+	parent.set_collision_mask_value(1, false)
+	parent.velocity.y = 50
+
+	await parent.get_tree().create_timer(0.15).timeout
+	parent.set_collision_mask_value(1, true)
