100750ea96395ac725f9cbbe072f8df8c70b19e2
JakubKrc <96569372+JakubKrc@users.noreply.github.com>
2026-04-10 19:41:35 +0200

    dialogue is component

---
+++ scripts/components/dialogue_trigger.gd
@@ -0,0 +1,27 @@
+extends Node
+
+@export_file("*.json") var dialogue_file: String = ""
+@export var add_to_interactable: bool = true
+@export var auto_trigger: bool = false
+
+var player_inside: bool = false
+
+func _ready():
+	if add_to_interactable:
+		get_parent().add_to_group("interactable")
+	get_parent().body_entered.connect(_on_body_entered)
+	get_parent().body_exited.connect(_on_body_exited)
+
+func _input(event):
+	if player_inside and event.is_action_pressed("use") and Global.can_player_move:
+		Global.dialogue.start(dialogue_file)
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		player_inside = true
+		if auto_trigger:
+			Global.dialogue.start(dialogue_file)
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		player_inside = false
